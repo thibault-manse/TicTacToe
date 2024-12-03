76efe4faa14329a6db258c262299f79378bcac8c
thibault-manse <thibault.manse@laplateforme.io>
2024-12-03 13:33:11 +0100

    Commentaire en anglais

--- TicTacToe menu.py
+++ TicTacToe menu.py
@@ -1,7 +1,7 @@
 import random
 
-retry = True #variable pour la boucle qui sert a rejouer
-replay = 1 #variable servant a r'avoir la demande pour rejouer ou non
+retry = True #variable for the loop which is used to replay
+replay = 1 #variable used to have the request to replay or not
 
 # Display the board
 def print_board(board):
@@ -23,17 +23,17 @@ def alternate_player(current_player):
     return "O" if current_player == "X" else "X"
 
 def game(board, current_player):
-    # Boucle pour définir les différents tours du jeu, avec un maximum de 9 tours
+    # Loop to define the different rounds of the game, with a maximum of 9 rounds
     cases = 0
     while cases < 9:
         print_board(board)
         try :
-            new_value = int(input(f"Joueur {current_player}, choisissez votre coup (1-9) : "))  # Utilisation de "new_value" pour choisir le coup du joueur
+            new_value = int(input(f"Joueur {current_player}, choisissez votre coup (1-9) : "))  # Using "new_value" to choose the player's move
         except ValueError:
             print("Valeur invalide, veillez reessayer !")
             continue
         
-        # Les différentes conditions à respecter pour enregistrer le choix du joueur actuel
+        # The different conditions to be respected to save the choice of the current player
         if 1 <= new_value <= 9 and board[new_value -1] == " ":
             board[new_value-1] = current_player
             cases += 1
@@ -41,33 +41,33 @@ def game(board, current_player):
             print("Coup invalide, réessayez.")
             continue
 
-        # Conditions pour gagner la partie
-        if ((board[0] == board[1] == board[2] != " ") or # Victoires par lignes
+        # Conditions to win the game
+        if ((board[0] == board[1] == board[2] != " ") or # Victories by lines
             (board[3] == board[4] == board[5] != " ") or
             (board[6] == board[7] == board[8] != " ") or
-            (board[0] == board[3] == board[6] != " ") or # Victoires par colonnes
+            (board[0] == board[3] == board[6] != " ") or # Victories by column
             (board[1] == board[4] == board[7] != " ") or
             (board[2] == board[5] == board[8] != " ") or
-            (board[0] == board[4] == board[8] != " ") or # Victoires par diagonales
+            (board[0] == board[4] == board[8] != " ") or # Victories by diagonals
             (board[2] == board[4] == board[6] != " ")):
             print_board(board)
-            print(f"Le joueur {current_player} gagne !") # Message de victoire
+            print(f"Le joueur {current_player} gagne !") # Victory message
             break
 
-        # Conditions pour un match nul
+        # Conditions for a draw
         if " " not in board:
             print_board(board)
-            print("Match nul !") # Message d'égalité
+            print("Match nul !") # Message of equality
             break
 
-        current_player = alternate_player(current_player) # Changer de joueur à la fin du tour
+        current_player = alternate_player(current_player) # Change player at end of round
 
-def start(retry, replay): #fonction du menu et du choix xe rejouer ou non
+def start(retry, replay): #function of the menu and the choice xe replay or not
     print("Tic Tac Toa")
     print("1 - Joueur vs Joueur")
     print("2 - Joueur vs IA")
     try : 
-        choice = int(input("Choisissez votre mode de jeux (1 ou 2): ")) #choix du mode de jeu
+        choice = int(input("Choisissez votre mode de jeux (1 ou 2): ")) #choice of game mode
     except ValueError:
         print("Valeur rentré incorrect !")
 
@@ -77,7 +77,7 @@ def start(retry, replay): #fonction du menu et du choix xe rejouer ou non
                 board = [" "," "," "," "," "," "," "," "," "] #Create a table with 9 cases
                 game(board, current_player)
                 replay = 0
-            val = str(input("Voulez vous rejouer ? (y ou n) : "))#choix de rejouer ou non
+            val = str(input("Voulez vous rejouer ? (y ou n) : "))#choice to play again or not
             if val != "y" and val !="n":
                 print("erreur d'input !")
             elif val == "y":
